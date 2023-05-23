c17394f642432695b0b215f78915adaa219383b8
Nixon <voxell.technologies@gmail.com>
2023-05-23 16:07:50 +0800

    add compute DepthToHeigth, AddWater func

--- Assets/Scripts/OmniTwin/FloodSimulation/FloodSimulation.cs
+++ Assets/Scripts/OmniTwin/FloodSimulation/FloodSimulation.cs
@@ -4,30 +4,63 @@ using Unity.Mathematics;
 
 public static class FloodSimulation
 {
+    public static ComputeShader cs_DepthToHeight { get; private set; }
     public static ComputeShader cs_AddWater { get; private set; }
     public static ComputeShader cs_CalculateWaterHeight { get; private set; }
-    public static ComputeShader cs_DepthToHeight { get; private set; }
     public static ComputeShader cs_PropagateWater { get; private set; }
 
     public static void Initialize()
     {
+        cs_DepthToHeight = Resources.Load<ComputeShader>("Computes/DepthToHeight");
         cs_AddWater = Resources.Load<ComputeShader>("Computes/AddWater");
         cs_CalculateWaterHeight = Resources.Load<ComputeShader>("Computes/CalculateWaterHeight");
-        cs_DepthToHeight = Resources.Load<ComputeShader>("Computes/DepthToHeight");
         cs_PropagateWater = Resources.Load<ComputeShader>("Computes/PropagateWater");
+    }
+
+    public static void Compute_DepthToHeight(
+        CommandBuffer cmd,
+        float height,
+        Texture tex_depth, GraphicsBuffer gb_heights
+    ) {
+        cmd.BeginSample("DepthToHeight");
+
+        int texWidth = tex_depth.width;
+        int texHeight = tex_depth.height;
+        cmd.SetComputeIntParams(cs_DepthToHeight, ShaderID._Dimension, texWidth, texWidth);
+        cmd.SetComputeFloatParam(cs_DepthToHeight, ShaderID._Height, height);
+
+        cmd.SetComputeTextureParam(cs_DepthToHeight, 0, ShaderID.tex_Depth, tex_depth);
+        cmd.SetComputeBufferParam(cs_DepthToHeight, 0, ShaderID.gb_Heights, gb_heights);
+
+        cmd.DispatchCompute(
+            cs_DepthToHeight, 0,
+            mathx.batch_count(texWidth, 8), mathx.batch_count(texHeight, 8), 1
+        );
+
+        cmd.EndSample("DepthToHeight");
     }
 
     public static void Compute_AddWater(
         CommandBuffer cmd,
         int count, float radius, float2 center, uint seed,
-        GraphicsBuffer gb_Height, GraphicsBuffer gb_WaterCoords
+        GraphicsBuffer gb_heights, GraphicsBuffer gb_waterCoords
     ) {
+        cmd.BeginSample("AddWater");
+
         cmd.SetComputeIntParam(cs_AddWater, ShaderID._ThreadCount, count);
         cmd.SetComputeFloatParam(cs_AddWater, ShaderID._Radius, radius);
         cmd.SetComputeVectorParam(cs_AddWater, ShaderID._Center, new Vector4(center.x, center.y, 0.0f, 0.0f));
         cmd.SetComputeIntParam(cs_AddWater, ShaderID._Seed, (int)seed);
 
-        cmd.DispatchCompute(cs_AddWater, 0, mathx.batch_count(count, 64), 1, 1);
+        cmd.SetComputeBufferParam(cs_AddWater, 0, ShaderID.gb_Heights, gb_heights);
+        cmd.SetComputeBufferParam(cs_AddWater, 0, ShaderID.gb_WaterCoords, gb_waterCoords);
+
+        cmd.DispatchCompute(
+            cs_AddWater, 0,
+            mathx.batch_count(count, 64), 1, 1
+        );
+
+        cmd.EndSample("AddWater");
     }
 
     /// <summary>Create depth render texture.</summary>
--- Assets/Scripts/OmniTwin/Util/ShaderID.cs
+++ Assets/Scripts/OmniTwin/Util/ShaderID.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public static class ShaderID
 {
     public static readonly int _ThreadCount = Shader.PropertyToID("_ThreadCount");
+    public static readonly int _Dimension = Shader.PropertyToID("_Dimension");
     public static readonly int _Radius = Shader.PropertyToID("_Radius");
+    public static readonly int _Height = Shader.PropertyToID("_Height");
     public static readonly int _Center = Shader.PropertyToID("_Center");
     public static readonly int _Seed = Shader.PropertyToID("_Seed");
     public static readonly int _RandomStrength = Shader.PropertyToID("_RandomStrength");
 
-    public static readonly int gb_Height = Shader.PropertyToID("gb_Height");
+    public static readonly int tex_Depth = Shader.PropertyToID("tex_Depth");
+    public static readonly int gb_Heights = Shader.PropertyToID("gb_Heights");
     public static readonly int gb_WaterCoords = Shader.PropertyToID("gb_WaterCoords");
 }
