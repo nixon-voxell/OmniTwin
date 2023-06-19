f667f63b9b512e287af138dab792cc50feff2c31
Nixon <voxell.technologies@gmail.com>
2023-06-19 17:30:55 +0800

    add screenshot btn & timer lbl

--- Assets/Scripts/OmniTwin/FloodSimulation/FloodVisualizationUI.cs
+++ Assets/Scripts/OmniTwin/FloodSimulation/FloodVisualizationUI.cs
@@ -9,6 +9,8 @@ namespace OmniTwin.UI
 
         private VisualElement m_Root;
         private VisualElement m_VisualizationImg;
+        private Label m_TimerLbl;
+        private Button m_ScreenshotBtn;
 
         public VisualElement VisualizationImg => this.m_VisualizationImg;
 
@@ -23,6 +25,8 @@ namespace OmniTwin.UI
 
             this.m_Root = this.m_Document.rootVisualElement;
             this.m_VisualizationImg = this.m_Root.Q<VisualElement>("visualization-img");
+            this.m_TimerLbl = this.m_Root.Q<Label>("timer-lbl");
+            this.m_ScreenshotBtn = this.m_Root.Q<Button>("screenshot-btn");
 
             // set visibility to false by default
             this.SetDocumentVisible(false);
