6e2d0c8afa46873c5e7928c4cf583275a2c1cbb9
Nixon <voxell.technologies@gmail.com>
2023-08-03 14:34:05 +0800

    feat: complete floodsim visualize inspection tool

--- Assets/Scripts/OmniTwin/FloodSimulation/FloodVisualizationUI.cs
+++ Assets/Scripts/OmniTwin/FloodSimulation/FloodVisualizationUI.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.Mathematics;
 using Voxell.Util;
 
 namespace OmniTwin.UI
@@ -26,76 +27,31 @@ namespace OmniTwin.UI
 
         [SerializeField, InspectOnly] private bool m_IsPlaying = false;
         private List<Screenshot> m_Screenshots;
+        private int m_ScreenshotIndex;
 
         public VisualElement SimulationImg => this.m_SimulationImg;
 
-        public void SetVisible(bool active)
+        private void OffsetVisualizationImg(int offset)
         {
-            this.m_Root.visible = active;
-
-            if (!active)
-            {
-                this.m_IsPlaying = false;
-            }
+            this.SetVisualizationImg(this.m_ScreenshotIndex + offset);
         }
 
-        private void Start()
+        private void SetVisualizationImg(int index)
         {
-            UXManager.Instance.FloodVisualizationUI = this;
+            this.m_ScreenshotIndex = math.clamp(
+                index,
+                0, this.m_Screenshots.Count - 1
+            );
 
-            this.m_Root = this.m_Document.rootVisualElement;
-            this.m_SimulationImg = this.m_Root.Q<VisualElement>("simulation-img");
-            this.m_VisualizationImg = this.m_Root.Q<VisualElement>("visualization-img");
+            Screenshot screenshot = this.m_Screenshots[this.m_ScreenshotIndex];
 
-            this.m_SimFrameLbl = this.m_Root.Q<Label>("simframe-lbl");
-            this.m_ViewFrameLbl = this.m_Root.Q<Label>("viewframe-lbl");
+            UIElementUtil.SetTexture(
+                this.m_VisualizationImg,
+                screenshot.tex_Screenshot
+            );
 
-            this.m_PauseBtn = this.m_Root.Q<Button>("pause-btn");
-            this.m_PlayBtn = this.m_Root.Q<Button>("play-btn");
-            this.m_ResetBtn = this.m_Root.Q<Button>("reset-btn");
-            this.m_ScreenshotBtn = this.m_Root.Q<Button>("screenshot-btn");
-
-            this.m_Screenshots = new List<Screenshot>(64);
-
-            this.m_VisualizationImg = this.m_Root.Q<VisualElement>("visualization-img");
-            this.m_PrevBtn = this.m_Root.Q<Button>("prev-btn");
-            this.m_NextBtn = this.m_Root.Q<Button>("next-btn");
-
-
-            // set visibility to false by default
-            this.SetVisible(false);
-
-            this.m_PauseBtn.clicked += () =>
-            {
-                this.m_IsPlaying = false;
-            };
-
-            this.m_PlayBtn.clicked += () =>
-            {
-                this.m_IsPlaying = true;
-            };
-
-            this.m_ResetBtn.clicked += () =>
-            {
-                this.m_IsPlaying = false;
-                this.ResetFloodSim();
-            };
-
-            this.m_ScreenshotBtn.clicked += () =>
-            {
-                this.ScreenshotFloodSim();
-            };
-        }
-
-        private void Update()
-        {
-            // update frame count label
-            this.m_SimFrameLbl.text = $"Frame: {this.m_FloodVisualization.FrameCount}";
-
-            if (this.m_IsPlaying)
-            {
-                this.m_FloodVisualization.UpdateSim();
-            }
+            this.m_ViewFrameLbl.text =
+            $"Frame: {screenshot.FrameCount} ({this.m_ScreenshotIndex + 1}/{this.m_Screenshots.Count})";
         }
 
         private void ResetFloodSim()
@@ -129,6 +85,89 @@ namespace OmniTwin.UI
                 FrameCount = this.m_FloodVisualization.FrameCount,
                 tex_Screenshot = tex_screenshot,
             });
+
+            this.SetVisualizationImg(this.m_Screenshots.Count - 1);
+        }
+
+        public void SetVisible(bool active)
+        {
+            this.m_Root.visible = active;
+
+            if (!active)
+            {
+                this.m_IsPlaying = false;
+            }
+        }
+
+        private void Start()
+        {
+            UXManager.Instance.FloodVisualizationUI = this;
+
+            this.m_Root = this.m_Document.rootVisualElement;
+            this.m_SimulationImg = this.m_Root.Q<VisualElement>("simulation-img");
+            this.m_VisualizationImg = this.m_Root.Q<VisualElement>("visualization-img");
+
+            this.m_SimFrameLbl = this.m_Root.Q<Label>("simframe-lbl");
+            this.m_ViewFrameLbl = this.m_Root.Q<Label>("viewframe-lbl");
+
+            // simulation buttons
+            this.m_PauseBtn = this.m_Root.Q<Button>("pause-btn");
+            this.m_PlayBtn = this.m_Root.Q<Button>("play-btn");
+            this.m_ResetBtn = this.m_Root.Q<Button>("reset-btn");
+            this.m_ScreenshotBtn = this.m_Root.Q<Button>("screenshot-btn");
+
+            // visualization buttons
+            this.m_PrevBtn = this.m_Root.Q<Button>("prev-btn");
+            this.m_NextBtn = this.m_Root.Q<Button>("next-btn");
+
+            this.m_Screenshots = new List<Screenshot>(64);
+            this.m_ScreenshotIndex = 0;
+
+
+            // set visibility to false by default
+            this.SetVisible(false);
+
+            this.m_PauseBtn.clicked += () =>
+            {
+                this.m_IsPlaying = false;
+            };
+
+            this.m_PlayBtn.clicked += () =>
+            {
+                this.m_IsPlaying = true;
+            };
+
+            this.m_ResetBtn.clicked += () =>
+            {
+                this.m_IsPlaying = false;
+                this.ResetFloodSim();
+            };
+
+            this.m_ScreenshotBtn.clicked += () =>
+            {
+                this.ScreenshotFloodSim();
+            };
+
+            this.m_PrevBtn.clicked += () =>
+            {
+                this.OffsetVisualizationImg(-1);
+            };
+
+            this.m_NextBtn.clicked += () =>
+            {
+                this.OffsetVisualizationImg(1);
+            };
+        }
+
+        private void Update()
+        {
+            // update frame count label
+            this.m_SimFrameLbl.text = $"Frame: {this.m_FloodVisualization.FrameCount}";
+
+            if (this.m_IsPlaying)
+            {
+                this.m_FloodVisualization.UpdateSim();
+            }
         }
     }
 
